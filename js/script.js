c40dc515d30e290551228c4193725b7724deb4fd
Roman <romansyplyviy@gmail.com>
2026-05-16 17:40:49 +0100

    feat: add input validation and local storage support

--- js/script.js
+++ js/script.js
@@ -552,11 +552,45 @@ dots.forEach((dot) => {
 
 const result = document.querySelector(".calculating__result span");
 
-let gender = "female",
-  height,
-  weight,
-  age,
+let gender, height, weight, age, levelActivity;
+
+if (localStorage.getItem("gender")) {
+  gender = localStorage.getItem("gender");
+} else {
+  gender = "female";
+  localStorage.setItem("gender", "female");
+}
+
+if (localStorage.getItem("levelActivity")) {
+  levelActivity = localStorage.getItem("levelActivity");
+} else {
   levelActivity = 1.375;
+  localStorage.setItem("levelActivity", 1.375);
+}
+
+function initLocalSettings(selector, activeClass) {
+  const elements = document.querySelectorAll(selector);
+
+  elements.forEach((elem) => {
+    elem.classList.remove(activeClass);
+    if (elem.getAttribute("id") === localStorage.getItem("gender")) {
+      elem.classList.add(activeClass);
+    }
+
+    if (
+      elem.getAttribute("data-activity") ===
+      localStorage.getItem("levelActivity")
+    ) {
+      elem.classList.add(activeClass);
+    }
+  });
+}
+
+initLocalSettings("#gender div", "calculating__choose-item_active");
+initLocalSettings(
+  ".calculating__choose_big div",
+  "calculating__choose-item_active",
+);
 
 function calcTotal() {
   if (!gender || !height || !weight || !age || !levelActivity) {
@@ -577,16 +611,21 @@ function calcTotal() {
 
 calcTotal();
 
-function getStaticInformation(parentSelector, activeClass) {
-  const elements = document.querySelectorAll(`${parentSelector} div`);
+function getStaticInformation(selector, activeClass) {
+  const elements = document.querySelectorAll(selector);
 
   // трюк делегування подій
   elements.forEach((elem) =>
     elem.addEventListener("click", (e) => {
       if (e.target.getAttribute("data-activity")) {
         levelActivity = +e.target.getAttribute("data-activity");
+        localStorage.setItem(
+          "levelActivity",
+          +e.target.getAttribute("data-activity"),
+        );
       } else {
         gender = e.target.getAttribute("id");
+        localStorage.setItem("gender", e.target.getAttribute("id"));
       }
 
       elements.forEach((elem) => {
@@ -600,9 +639,9 @@ function getStaticInformation(parentSelector, activeClass) {
   );
 }
 
-getStaticInformation("#gender", "calculating__choose-item_active");
+getStaticInformation("#gender div", "calculating__choose-item_active");
 getStaticInformation(
-  ".calculating__choose_big",
+  ".calculating__choose_big div",
   "calculating__choose-item_active",
 );
 
@@ -610,6 +649,12 @@ function getDynamicInformation(selector) {
   const input = document.querySelector(selector);
 
   input.addEventListener("input", () => {
+    if (input.value.match(/\D/g)) {
+      input.style.border = "1px solid red";
+    } else {
+      input.style.border = "none";
+    }
+
     switch (input.getAttribute("id")) {
       case "height":
         height = +input.value;
